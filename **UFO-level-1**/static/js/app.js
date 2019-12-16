4d6a783fced8a760d545ca79a793f0ac6969d5a6
jabruhns <bruhnsjames@gmail.com>
2019-12-15 22:30:31 -0800

    pt1

--- **UFO-level-1**/static/js/app.js
+++ **UFO-level-1**/static/js/app.js
@@ -2,3 +2,37 @@
 var tableData = data;
 
 // YOUR CODE HERE!
+
+
+var tbody = d3.select("tbody")
+data.forEach((data) => {
+    var row = tbody.append("tr");
+    Object.entries(data).forEach(([key, value]) => {
+        var cell = row.append("td");
+        cell.text(value);
+    });
+});
+
+
+d3.select("#filter-btn").on("click", function() {
+    var input = d3.select(".form-control").property("value")
+    console.log(input)
+    var filteredData = tableData.filter(value => value.datetime === input)
+    tbody.html("")
+    filteredData.forEach((data) => {
+        var row = tbody.append("tr");
+        Object.entries(data).forEach(([key, value]) => {
+            var cell = row.append("td");
+            cell.text(value);
+        });
+    });
+})
+
+// var date = filteredData.map(value => value.datetime)
+// var city = filteredData.map(value => value.city)
+// var state = filteredData.map(value => value.state)
+// var country = filteredData.map(value => value.country)
+// var shape = filteredData.map(value => value.shape)
+// var duration = filteredData.map(value => value.durationMinutes)
+// var comments = filteredData.map(value => value.comments)
+// console.log(city)
